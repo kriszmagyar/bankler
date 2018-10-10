3e605ab495293dd151786432de8707ce7aa3f265
Krisz Magyar <krisz.magyar@gmail.com>
2018-10-10 23:27:31 +0200

    addAccount

--- frontend/src/views/app/AccountsView.js
+++ frontend/src/views/app/AccountsView.js
@@ -5,26 +5,85 @@ import { withRouter } from 'react-router-dom'
 import { updateAccounts } from '../../store/actions'
 import { instance as axios } from '../../axios'
 import { isEmptyObj } from '../../utility'
+import { ui } from '../../main/js/ui'
 
 import { Account } from '../../components/Account/Account'
 import { Loader } from '../../components/Loader'
+import { Button } from '../../components/UI/Button'
+import { InputText } from '../../components/UI/Inputs'
 
 class AccountsView extends Component {
 
-    goToAccount = id => {
-        this.props.history.push('/accounts/' + id)
+    state = {
+        number: 200
+    }
+
+    handleChange = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    // componentDidMount() {
+        
+    //     axios({
+    //         method: 'get',
+    //         url: '/account',
+    //         params: this.state
+    //     })
+    //     .then(res => {
+    //         console.log(res)
+    //         this.auth()
+    //     })
+    //     .catch(err => {
+    //         console.log(err.response)
+    //         ui.alert({
+    //             content: err.response.data,
+    //             type: 'danger'
+    //         })
+    //     })
+
+    // }
+
+    addAccount() {
+
+        axios({
+            method: 'post',
+            url: '/account',
+            params: this.state
+        })
+        .then(res => {
+            console.log(res)
+        })
+        .catch(err => {
+            console.log(err.response)
+            ui.alert({
+                content: err.response.data,
+                type: 'danger'
+            })
+        })
+
     }
     
     render() {
+
+        const Form = (
+            <form className='form small' onSubmit={this.handleSubmit} noValidate>
+                <InputText
+                    label='Number'
+                    type='text'
+                    name='number'
+                    placeholder='Number'
+                    onChange={this.handleChange}
+                    value={this.state.number}
+                />
+                <Button text='Add account' onClick={this.addAccount} />
+            </form>
+        )
     
         return (
             <div className='container'>
-                <Account 
-                    name = 'My Account'
-                    currency = 'USD'
-                    cash = { 200000 }
-                    number = '0000-0000-0000'
-                />
+                { Form }
             </div>
         )
     }
